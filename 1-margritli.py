89f6771511b26d0023b380c13d403fb61da002d3
Matthias WaltherBueel <youremail@yourdomain.comgit config --global user.name Your>
2022-12-07 11:36:44 +0100

    task1 dimension are not coorect

--- 1-margritli.py
+++ 1-margritli.py
@@ -4,13 +4,29 @@ Erzeuge eine Grafik eines Gänseblümchens
 
 from pytamaro.de import (
     Grafik,
-    weiss, schwarz, rgb_farbe,
+    weiss, schwarz, rgb_farbe,gelb,
     ellipse, leere_grafik,
-    fixiere, kombiniere, drehe,
+    fixiere, kombiniere, drehe,ueberlagere,
     zeige_grafik, speichere_grafik,
 )
 
 GELB = rgb_farbe(255, 206, 46)
 
 def margritli(radius: float) -> Grafik:
-    pass
+    alle_blaetter= leere_grafik()
+    for i in range(1,13):
+        alle_blaetter = kombiniere(
+                alle_blaetter,
+                drehe(i*30,fixiere(
+                    "mitte", "unten", 
+                    ellipse(radius*2,radius*8,weiss)
+                    )
+                    )
+                )
+    bluete= ellipse(radius*4,radius*4,gelb)
+    return ueberlagere(bluete,alle_blaetter)
+            
+
+     
+zeige_grafik(margritli(40))
+
