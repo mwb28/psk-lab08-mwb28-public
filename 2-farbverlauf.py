9b012480b9606acb160c8c40388b5916e4f3d25d
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-12-08 16:05:27 +0100

    task3 not the best solution

--- 2-farbverlauf.py
+++ 2-farbverlauf.py
@@ -22,12 +22,14 @@ def zeichne_blaetter(anzahl_blaetter: int, radius: int)->list[Grafik]:
     alle_blaetter = []
     for i in range(1,anzahl_blaetter+1):
         alle_blaetter.append(drehe(i*(360/anzahl_blaetter),
-        fixiere("mitte","unten",ellipse(radius*2,radius*8,hsl_farbe(i*(360/anzahl_blaetter),1,0.5,0.5)))))
+        fixiere("mitte","unten",
+            ellipse(radius*2,radius*8,
+                hsl_farbe(i*(360/anzahl_blaetter),1,0.5,0.5)))))
     return alle_blaetter
 
 def kombinere_blaetter(blaetter : list[Grafik])-> Grafik:
     return reduziere(blaetter,leere_grafik(),lambda x , y: kombiniere(x,y))
 
-zeige_grafik(kombinere_blaetter(zeichne_blaetter(12,30)))
+zeige_grafik(kombinere_blaetter(zeichne_blaetter(65,30)))
 
 
--- 3-balkendiagramm.py
+++ 3-balkendiagramm.py
@@ -2,21 +2,73 @@
 Erzeuge Grafik eines Balkendiagramms der animals Daten
 """
 from pytamaro.de import (
-    Grafik,
-    magenta, rot, blau,
+    Grafik, Farbe,
+    magenta, rot, blau,transparent,
     text, rechteck, ellipse, leere_grafik,
     grafik_breite, grafik_hoehe,
-    neben, fixiere, kombiniere,
+    neben, fixiere, kombiniere, ueber,
     zeige_grafik, speichere_grafik,
 )
 from csv import DictReader
 
 GESCHLECHTS_FARBEN = {"female": rot, "male": blau, "hermaphrodite": magenta}
 
+# def reduziere(elemente: list, neutrales_element, f):
+#     resultat = neutrales_element
+#     for element in elemente:
+#         resultat = f(resultat, element)
+#     return resultat
+
 def balken_diagramm(balken_hoehe: float) -> Grafik:
     # öffne die Datei zum Lesen (read, r)
     file = open("animals.csv", "r")
     # erzeuge einen CSV Dictionary Reader, der die Datei einliest und durch die Zeilen iterieren kann
     leser = DictReader(file, delimiter=",")
-    #...
-    return neben(label_spalte, balken_spalte)
+    
+    abstand_vertikal = rechteck(balken_hoehe,balken_hoehe/3,transparent)
+    abstand_balken = rechteck(balken_hoehe,balken_hoehe/9,transparent)
+    label_spalte = leere_grafik()
+    balken_spalte = leere_grafik()
+    farbe = magenta
+    for tier in leser:
+        if (tier["Sex"]== "female"):
+            farbe = rot
+        elif(tier["Sex"]== "male"):
+            farbe = blau
+        else: 
+            farbe = magenta
+        
+        label_spalte= kombiniere(fixiere("rechts","unten",label_spalte),
+                    fixiere("rechts","oben",
+                    kombiniere(fixiere("rechts", "unten",abstand_vertikal),
+                    fixiere("rechts", "oben",text(tier["Name"],"Arial",balken_hoehe,farbe))
+                    )))
+        balken_spalte = kombiniere(fixiere("links","unten",balken_spalte),
+                        fixiere("links","oben",
+                        kombiniere(fixiere("links","unten",abstand_balken),
+                        fixiere("links","oben",zeichne_anzahl_kreise(int(tier["Legs"]),balken_hoehe,farbe))
+                        )
+                        )
+        
+        
+        )
+        
+    return neben(label_spalte,neben(abstand_vertikal,balken_spalte))
+
+
+
+
+
+def zeichne_anzahl_kreise(anzahl_kreise: int, balken_hoehe: float, farbe: Farbe)-> Grafik:
+
+    abstand = rechteck(balken_hoehe/4,balken_hoehe/4,transparent)
+    zeichne_kreise = leere_grafik()
+    for i in range(anzahl_kreise):
+        zeichne_kreise= neben(zeichne_kreise,neben(ellipse(balken_hoehe,balken_hoehe,farbe),abstand))
+    if anzahl_kreise == 0:
+        return rechteck(balken_hoehe,balken_hoehe,transparent)
+    else: return zeichne_kreise
+
+
+zeige_grafik(balken_diagramm(39))
+
