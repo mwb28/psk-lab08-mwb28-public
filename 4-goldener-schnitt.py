0ade0cee873f4816b3dd3f3d107e1e97d9753ecc
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-12-08 22:41:32 +0100

    taks4 with lambda

--- 4-goldener-schnitt.py
+++ 4-goldener-schnitt.py
@@ -14,12 +14,40 @@ GRUEN = rgb_farbe(0, 161, 110)
 GELB = rgb_farbe(255, 206, 46)
 
 GOLDENER_SCHNITT = (sqrt(5) - 1) / 2
+
+def reduziere(elemente: list, neutrales_element, f):
+    resultat = neutrales_element
+    for element in elemente:
+        resultat = f(resultat, element)
+    return resultat
+
+def bluetenstand2(drehung: float, anzahl : int)-> Grafik:
+    drehung = 360*drehung
+    blueten = [drehe(drehung*i+1,kombiniere(
+                fixiere("rechts","mitte",rechteck(sqrt(i)*80+20,1,transparent)),
+                fixiere("links","mitte",ellipse(50,50,GELB))))
+                for i in range(anzahl)]
+    return reduziere(blueten,leere_grafik(),lambda x,y: ueberlagere(x,y))        
+
+zeige_grafik(bluetenstand2(GOLDENER_SCHNITT,400))
+
+
+
+
+
+
+
+
+
+
+
+
 def bluetenstand(drehung: float, anzahl: int) -> Grafik:
     zeichne_blueten = leere_grafik()
     
     drehung = 360*drehung
     for i in range(anzahl):
-        kreis = ellipse(180,180,hsl_farbe(i*(360/anzahl),1,0.5,0.5))
+        kreis = ellipse(60,60,hsl_farbe(i*(360/anzahl),1,0.5,0.5))
         abstand = sqrt(i)*80+20
         blueten_mit_abstand = kombiniere(
                             fixiere("rechts","mitte",rechteck(abstand,1,transparent)),
@@ -28,6 +56,10 @@ def bluetenstand(drehung: float, anzahl: int) -> Grafik:
     hintergrund = rechteck(1800,1800,schwarz)
     return ueberlagere(zeichne_blueten,hintergrund)
 
-zeige_grafik(bluetenstand(GOLDENER_SCHNITT,400))
+# zeige_grafik(bluetenstand(GOLDENER_SCHNITT,400))
+
+def blaetter_rosette(drehung: float, blatt_radius: float, abstands_faktor: float, anzahl: int) -> Grafik:
+
+    pass
 
 # speichere_gif("bluetenstand",[bluetenstand(GOLDENER_SCHNITT + schritt*0.00001, 400) for schritt in range(200)],100)
