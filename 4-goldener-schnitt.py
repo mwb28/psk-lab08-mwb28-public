fe1e2046a5fb77c772865afa8e49011773552070
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-12-14 18:23:35 +0100

    add contestflower

--- 4-goldener-schnitt.py
+++ 4-goldener-schnitt.py
@@ -15,51 +15,61 @@ GELB = rgb_farbe(255, 206, 46)
 
 GOLDENER_SCHNITT = (sqrt(5) - 1) / 2
 
+
 def reduziere(elemente: list, neutrales_element, f):
     resultat = neutrales_element
     for element in elemente:
         resultat = f(resultat, element)
     return resultat
 
-def bluetenstand2(drehung: float, anzahl : int)-> Grafik:
+def bluetenstand(drehung: float, anzahl : int, farbe : Farbe, groesse_bluete : float)-> Grafik:
     drehung = 360*drehung
-    blueten = [drehe(drehung*i+1,kombiniere(
-                fixiere("rechts","mitte",rechteck(sqrt(i)*80+20,1,transparent)),
-                fixiere("links","mitte",ellipse(50,50,GELB))))
+    blueten = [drehe(drehung*i,kombiniere(
+                fixiere("rechts","mitte",rechteck(sqrt(i)*80 +100 ,1,transparent)),
+                fixiere("links","mitte",ellipse(groesse_bluete,groesse_bluete,farbe))))
                 for i in range(anzahl)]
     return reduziere(blueten,leere_grafik(),lambda x,y: ueberlagere(x,y))        
 
-zeige_grafik(bluetenstand2(GOLDENER_SCHNITT,400))
+# zeige_grafik(bluetenstand2(GOLDENER_SCHNITT,400))+
 
 
 
-
-
-
-
-
-
-
-
-
-def bluetenstand(drehung: float, anzahl: int) -> Grafik:
+def bluetenstand_farbig(drehung: float, anzahl: int, groesse_bluete : float) -> Grafik:
     zeichne_blueten = leere_grafik()
     
     drehung = 360*drehung
     for i in range(anzahl):
-        kreis = ellipse(60,60,hsl_farbe(i*(360/anzahl),1,0.5,0.5))
-        abstand = sqrt(i)*80+20
+        kreis = ellipse(groesse_bluete,groesse_bluete,hsl_farbe(i*(60/anzahl),1,0.5,1))
         blueten_mit_abstand = kombiniere(
-                            fixiere("rechts","mitte",rechteck(abstand,1,transparent)),
+                            fixiere("rechts","mitte",rechteck(sqrt(i)*10+10,1,transparent)),
                             fixiere("links","mitte",kreis))
         zeichne_blueten = ueberlagere(drehe(drehung*i+1,blueten_mit_abstand),zeichne_blueten) 
-    hintergrund = rechteck(1800,1800,schwarz)
-    return ueberlagere(zeichne_blueten,hintergrund)
+    # hintergrund = rechteck(1800,1800,schwarz)
+    return zeichne_blueten
 
 # zeige_grafik(bluetenstand(GOLDENER_SCHNITT,400))
 
-def blaetter_rosette(drehung: float, blatt_radius: float, abstands_faktor: float, anzahl: int) -> Grafik:
 
-    pass
+
+
+def zeichne_blatt(blattradius: float, farbe : Farbe)-> Grafik:
+    blattteil_vorne = ueberlagere(drehe(150,kreis_sektor(blattradius,120,farbe)),
+                        drehe(330,kreis_sektor(blattradius,120,farbe)) ) 
+    blatteil_hinten = ueberlagere(drehe(150,kreis_sektor(blattradius+5,120,schwarz)),
+                        drehe(330,kreis_sektor(blattradius+5,120,schwarz))  ) 
+  
+    return ueberlagere(blattteil_vorne,blatteil_hinten)
+
+ 
+   
+def blaetter_rosette(drehung: float, anzahl : int, blattradius : float)-> Grafik:
+    drehung = 360*drehung
+    blaetter = [drehe(drehung*i,kombiniere(
+                fixiere("rechts","mitte",rechteck(sqrt(i)*58+125,1,transparent)),
+                fixiere("links","mitte",zeichne_blatt(blattradius,hsl_farbe(i*(120/anzahl),0.5,0.5,1)))))
+                for i in range(anzahl)]
+    return reduziere(blaetter,leere_grafik(),lambda x,y: ueberlagere(x,y))  
+
+speichere_grafik("wettbewerbsblume", ueberlagere(bluetenstand_farbig(GOLDENER_SCHNITT,400,20),blaetter_rosette(GOLDENER_SCHNITT,30,90)))
 
 # speichere_gif("bluetenstand",[bluetenstand(GOLDENER_SCHNITT + schritt*0.00001, 400) for schritt in range(200)],100)
